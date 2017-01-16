fa9f108fae664ccbb8255b9aee03fb1b5111cd75
Rasmus Précenth <rasmus@precenth.eu>
2017-01-16 20:45:51 +0100

    It's better this way.

--- wrapper/src/main/java/example/Wrapper.java
+++ wrapper/src/main/java/example/Wrapper.java
@@ -1,3 +1,7 @@
+/*
+ * This is an example adapted from the neo4j docs where the logic is moved to
+ * Eta.
+ */
 package example;
 
 /*
@@ -27,5 +31,17 @@ public class Wrapper
         }
 
         return e.fib(n.intValue());
+    }
+
+    @UserFunction
+    @Description("example.join(['s1','s2',...], delimiter) - join the given strings with the given delimiter.")
+    public String join(
+            @Name("strings") List<String> strings,
+            @Name(value = "delimiter", defaultValue = ",") String delimiter) {
+        if (strings == null || delimiter == null) {
+            return null;
+        }
+        String[] aStrings = strings.toArray(new String[strings.size()]);
+        return e.join(delimiter, aStrings);
     }
 }
